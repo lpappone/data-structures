5b8aa93a25e72d0bcf37c8614287abd29cedb63b
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-07-23 12:21:17 -0700

    Completed implementation of functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,40 @@ var makeQueue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    var tempArray = [];
+
+    for(var key in storage){
+      tempArray.push(key)
+    }
+    var numberOfKeys = tempArray.length;
+
+    storage[numberOfKeys] = value
   };
 
   someInstance.dequeue = function(){
+    for(var key in storage){
+      storage[parseInt(key)-1] = storage[key]
+      delete storage[key]
+    }
+
+    var returnValue = storage[-1];
+    delete storage[-1];
+    return returnValue
   };
 
   someInstance.size = function(){
+    var tempArray = [];
+    console.log(storage)
+
+    if(storage[0]){
+      for(var key in storage){
+        tempArray.push(key);
+      }
+      console.log(tempArray)
+      return tempArray.length
+    }else{
+      return 0;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,38 @@ var makeStack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    // add value to first index in object
+    // push all other values up one in index
+    for(var key in storage){
+      storage[parseInt(key)+1] = storage[key]
+    }
+
+    storage[0] = value;
   };
 
   someInstance.pop = function(){
+    for(var key in storage){
+      storage[parseInt(key)-1] = storage[key]
+      delete storage[key]
+    }
+
+    var returnValue = storage[-1];
+    delete storage[-1];
+    return returnValue
   };
 
   someInstance.size = function(){
+    var tempArray = [];
+
+    if(storage[0]){
+      for(key in storage){
+        tempArray.push(key);
+      }
+      return tempArray.length
+    }else{
+      return 0;
+    }
+
   };
 
   return someInstance;
