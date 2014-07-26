0906401a9dfea798ee6ebd8aa1ce060707794d01
lpappone <lpappone@gmail.com>
2014-07-25 22:01:51 -0700

    started graphs

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,19 +1,34 @@
 var Graph = function(){
+
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  this.NewNode = newNode;
+  this.edge = toNode;
+  console.log(this)
 };
 
 Graph.prototype.contains = function(node){
+  result = false;
+  for (key in this) {
+    if (this[key] === node) {result = true}
+  }
+return result;
 };
 
 Graph.prototype.removeNode = function(node){
+  for (key in this) {
+    if (this[key] === node) {
+      this[key] = null;
+    }
+  }
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
