72027672c846c659e1fe7fcbeecc14efb9c39b99
lpappone <lpappone@gmail.com>
2014-07-26 10:31:45 -0700

    implemented addNode, contains, removeNode, getEdge

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,33 +1,43 @@
 var Graph = function(){
-
+  this._storage = {};
+  this._counter = 0;
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
-
-  this[newNode] = [toNode]
-  console.log(this)
+  var counter = 0;
+  var existingNode;
+  for (var key in this._storage) {
+    counter++;
+    existingNode = key;
+    console.log(existingNode)
+  }
+  if (counter === 1) {
+    this._storage[newNode] = [existingNode];
+    this._storage[existingNode] = [newNode]
+  }
+  this._storage[newNode] = [toNode]
+  if(toNode){
+    this._storage[toNode].push(newNode)
+  }
+  console.log(this._storage[newNode])
 };
 
 Graph.prototype.contains = function(node){
 
-  // result = false;
-  // for (var key in this) {
-  //   if (key === node) {result = true}
-  // }
-  // return result; 
-  
-  return this[node] !== undefined;
+  return this._storage[node] !== undefined;
 };
 
 Graph.prototype.removeNode = function(node) {
-  // for (var key in this) {
-    // if (key === node) {
-      delete this[node];
+  delete this._storage[node];
 
- 
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  if(this._storage[fromNode].indexOf(toNode) > -1 ){
+    return true
+  }else{
+    return false
+  }
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
