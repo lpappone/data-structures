9b15d9e5bc0c923fb030433bef83f68787d2a4e7
lpappone <lpappone@gmail.com>
2014-07-25 20:59:01 -0700

    breaking hash tables

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,68 @@
 var makeBinarySearchTree = function(value){
+  var bST = {};
+  bST.value = value;
+  bST.left = null;
+  bST.right = null;
+  _.extend(bST, binaryMethods)
+
+  return bST
 };
 
+var binaryMethods = {};
+
+binaryMethods.insert = function(newValue){
+  //check if newValue is less than bST.value
+  if(newValue < this.value){
+    //check if no bST.left
+    if(this.left === null){
+      this.left = makeBinarySearchTree(newValue);
+    //if there is bST.left
+    }else{
+      this.left.insert(newValue)
+    }
+  }else{
+    //check if no bST.right
+    if(this.right === null){
+      this.right = makeBinarySearchTree(newValue);
+  //if there is bST.right
+    }else{
+      this.right.insert(newValue)
+    }
+  }
+
+}
+
+binaryMethods.contains = function(match) {
+  var result = false;
+  var searchTree = function(tree) {
+    if (match === tree.value) {
+      result = true
+    } else if (match > tree.value && (tree.right !==null)) {
+      searchTree(tree.right);
+    } else if (match < tree.value && (tree.left !==null)) {
+      searchTree(tree.left);
+    }
+    return result;
+  };
+  return searchTree(this);
+}
+
+binaryMethods.depthFirstLog = function(callback){
+  // if no left and no right
+  if(this.left === null && this.right === null){
+    callback(this)
+    //run callback on node
+  }else{
+    //make sure that left or right exists
+    this.left.depthFirstLog(callback);
+    this.right.depthFirstLog(callback)
+  }
+
+
+
+
+
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,21 +1,85 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._newStore;
+  this._counter = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  // console.log(i, "i" + "    " + this._limit)
+  if(this._storage.get(i) === undefined){
+    this._storage.set(i, [k,v]);
+    this._counter++;
+  }else{
+    this._storage.get(i).push(k,v);
+    this._counter++;
+  }
+
+  if(this._counter >= this._limit*.75){
+
+    this.increaseSize();
+  }
+
 };
+
+
+HashTable.prototype.increaseSize = function(){
+
+    this._limit *= 2;
+
+    this._newStore = this._storage;
+    this._storage = makeLimitedArray(this.limit)
+
+    for(var key in this._newStore){
+      this.insert(key, this._newStore[key])
+    }
+}
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  console.log(k)
+  console.log(this._storage)
+  console.log(this._storage.get(i))
+  var currentItem = this._storage.get(i);
+  for(var j = 0; j < currentItem.length; j++){
+    if(currentItem[j] === k){
+      return currentItem[j+1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var currentItem = this._storage.get(i);
+  var toDelete;
+  for(var j = 0; j < currentItem.length; j++){
+    if(currentItem[j] === k){
+      toDelete = currentItem[j+1];
+      console.log(currentItem[j+1])
+      currentItem[j+1] = null;
+      console.log(currentItem[j+1])
+      this._counter--
+    }
+  }
 
+  if (this._counter <= this._limit*.75) {
+    this.decreaseSize()
+  }
+  return toDelete;
 };
 
+
+HashTable.prototype.decreaseSize = _.once(function(){
+    this._limit /= 2;
+
+    this._newStore = this._storage;
+    this._storage = makeLimitedArray(this.limit)
+
+    for(var key in this._newStore){
+      this.insert(key, this._newStore[key])
+    }
+})
 
 
 /*
