e407f9a0dc2bd648ad5293b55ddcab13b71eb589
Lauren <lpappone@gmail.com>
2014-07-26 10:08:00 -0700

    removed loops

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,12 +2,26 @@ var Graph = function(){
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  this[newNode] = [toNode]
+  console.log(this)
 };
 
 Graph.prototype.contains = function(node){
+  // result = false;
+  // for (var key in this) {
+  //   if (key === node) {result = true}
+  // }
+  // return result; 
+  
+  return this[node] !== undefined;
 };
 
-Graph.prototype.removeNode = function(node){
+Graph.prototype.removeNode = function(node) {
+  // for (var key in this) {
+    // if (key === node) {
+      delete this[node];
+    // }
+  // }  
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
